f1c7d963c17205cc27d3158a2716809de3cf8515
Alexander Kozko <Alexander Kozko>
2017-10-08 20:43:22 +0500

    introduce wrapper for encapsulate process

--- MyStem.Sharp/Lemmatizer.cs
+++ MyStem.Sharp/Lemmatizer.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 using Newtonsoft.Json.Linq;
 
@@ -12,38 +10,23 @@ namespace MyStem.Sharp
     public class Lemmatizer: IDisposable
     {
         private readonly Regex _cleanRegex;
-        private static readonly object LockObject = new object();
-        private readonly Process _myStemProcess;
-        private readonly StreamReader _streamReader;
+        private readonly ConsoleProcessWrapper _processWrapper;
 
         public Lemmatizer(string path)
         {
             _cleanRegex = new Regex(@"[^\w\d\p{P}]", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-            _myStemProcess = CreateProcess(path);
-            _streamReader = new StreamReader(_myStemProcess.StandardOutput.BaseStream, Encoding.UTF8);
+            _processWrapper = new ConsoleProcessWrapper(startInfo:CreateStartInfo(path));
         }
 
         public WordDefenition[] Lemmatize(string text)
         {
             var cleanText = _cleanRegex.Replace(text ?? ""," ");
 
-            string result;
-            lock (LockObject)
-            {
-                result = GetProcessOutput(_myStemProcess, _streamReader, cleanText);
-            }
+            var result = _processWrapper.GetProcessOutput(cleanText);
             return JArray.Parse(result)
                 .ToObject<List<WordDefenition>>()
                 .Select(def => new WordDefenition(def.Text.Replace("\n", "").Replace(@"\s", ""), def.Analysis))
                 .ToArray();
-        }
-
-        private string GetProcessOutput(Process myStemProcess, StreamReader streamReader, string text)
-        {
-            var buffer = Encoding.UTF8.GetBytes(text + "\r\n");
-            myStemProcess.StandardInput.BaseStream.Write(buffer, 0, buffer.Length);
-            myStemProcess.StandardInput.BaseStream.Flush();
-            return streamReader.ReadLine();
         }
 
         private ProcessStartInfo CreateStartInfo(string path)
@@ -61,17 +44,9 @@ namespace MyStem.Sharp
             };
         }
 
-        private Process CreateProcess(string path)
-        {
-            var process = new Process {StartInfo = CreateStartInfo(path)};
-            process.Start();
-            return process;
-        }
-
         public void Dispose()
         {
-            _myStemProcess?.Dispose();
-            _streamReader?.Dispose();
+            _processWrapper?.Dispose();
         }
     }
 }
